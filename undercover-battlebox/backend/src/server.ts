c8422e5b4ba23322407dc4d6e0ac2892c577b5af
TIXOCEO <famgoldenbelt@gmail.com>
2025-11-15 20:25:18 +0100

    Update server.ts

--- undercover-battlebox/backend/src/server.ts
+++ undercover-battlebox/backend/src/server.ts
@@ -513,6 +513,7 @@ initDB().then(async () => {
     console.log("Connecting TikTok with saved host:", host);
     const { conn } = await startConnection(host, () => {});
     tiktokConn = conn;
+    sharedTikTokConn = conn;
 
     initGiftEngine(conn);
     initChatEngine(conn);
