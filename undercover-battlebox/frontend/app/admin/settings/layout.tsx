424c3f8d73276466044849bd613d77132ff1ecf7
TIXOCEO <famgoldenbelt@gmail.com>
2025-11-24 21:23:08 +0100

    Update layout.tsx

--- undercover-battlebox/frontend/app/admin/settings/layout.tsx
+++ undercover-battlebox/frontend/app/admin/settings/layout.tsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 export default function SettingsLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
