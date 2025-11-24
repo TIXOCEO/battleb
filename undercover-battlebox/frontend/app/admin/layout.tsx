5bb89abac124aaea8819584bda4ffa3b9a5b51bb
TIXOCEO <famgoldenbelt@gmail.com>
2025-11-24 21:27:06 +0100

    Update layout.tsx

--- undercover-battlebox/frontend/app/admin/layout.tsx
+++ undercover-battlebox/frontend/app/admin/layout.tsx
@@ -1,6 +1,6 @@
-"use client";
 export const dynamic = "force-dynamic";
 export const fetchCache = "force-no-store";
+"use client";
 
 import type { ReactNode } from "react";
 import Link from "next/link";
