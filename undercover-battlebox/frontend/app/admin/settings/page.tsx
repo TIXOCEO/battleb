fa98c24ae5ad617a5217ae916a943bd36e386e6a
TIXOCEO <famgoldenbelt@gmail.com>
2025-11-24 20:42:10 +0100

    Update page.tsx

--- undercover-battlebox/frontend/app/admin/settings/page.tsx
+++ undercover-battlebox/frontend/app/admin/settings/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export const fetchCache = "force-no-store";
+export const dynamicParams = false;
 
 import React, { useEffect, useState, useRef } from "react";
 import { getAdminSocket } from "@/lib/socketClient";
