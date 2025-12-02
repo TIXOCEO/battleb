8c12d676d875a5f75b6a94374f65c7702d113f2a
TIXOCEO <famgoldenbelt@gmail.com>
2025-12-02 21:14:44 +0100

    Delete undercover-battlebox/frontend/types directory

--- undercover-battlebox/frontend/types/EmitAdminFn.ts
+++
@@ -1,8 +0,0 @@
-import type { AdminSocketOutbound } from "@/lib/adminTypes";
-
-export type EmitAdminFn = <
-  E extends keyof AdminSocketOutbound
->(
-  event: E,
-  payload: Parameters<AdminSocketOutbound[E]>[0]
-) => void;
