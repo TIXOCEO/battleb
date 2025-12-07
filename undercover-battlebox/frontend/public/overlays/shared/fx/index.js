fc45dfd1dfce3ebdf7dc80fe8b6d17936be738fa
TIXOCEO <famgoldenbelt@gmail.com>
2025-12-08 00:35:43 +0100

    Add index.js to export various FX modules

---
+++ undercover-battlebox/frontend/public/overlays/shared/fx/index.js
@@ -0,0 +1,8 @@
+export { default as MoneyGunFX } from "./MoneyGunFX.js";
+export { default as DiamondBlastFX } from "./DiamondBlastFX.js";
+export { default as BombFX } from "./BombFX.js";
+export { default as CountdownFX } from "./CountdownFX.js";
+export { default as TargetPulseFX } from "./TargetPulseFX.js";
+export { default as VictimBlastFX } from "./VictimBlastFX.js";
+export { default as SurvivorShieldFX } from "./SurvivorShieldFX.js";
+export { default as GalaxyFX } from "./GalaxyFX.js";
